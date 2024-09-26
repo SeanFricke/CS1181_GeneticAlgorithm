675f143db56706db8d3b64abb5adba0d273aa84d
Sean Fricke <seanfricke03@gmail.com>
2024-09-26 08:51:44 -0400

    Begin the algorithm instructions in main.

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Chromosome class, for storing a list of items, and included training functions such as
+ * crossover of two Chromosomes, or mutation for a single one.
+ */
 public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome> {
     private static Random rng = new Random();
+
+    /**
+     * Construct an empty chromosome.
+     */
     public Chromosome() {
     }
 
+    /**
+     * Construct a chromosome using an ArrayList of items as its structure.
+     * @param items Structure template for the new chromosome.
+     */
     public Chromosome(ArrayList<Item> items) {
         for (Item item : items) {
             Item itemCopy = new Item(item.toString(), item.getWeight(), item.getValue());
@@ -15,6 +27,12 @@ public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome
 
     }
 
+    /**
+     * Create a 'child' chromosome between two parents (this chromosome, and one other)
+     * @param other Second chromosome object - along with this chromosome - that will give some of their traits
+     *              to the child.
+     * @return Child chromosome with random traits from each parent.
+     */
     public Chromosome crossover(Chromosome other) {
         Chromosome child = new Chromosome();
 
@@ -35,6 +53,9 @@ public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome
         return child;
     }
 
+    /**
+     * Give this chromosome random inverses of traits at a rare chance.
+     */
     public void mutate() {
         for (Item item : this) {
             int mutation = new Random().nextInt(9) + 1;
@@ -44,6 +65,11 @@ public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome
         }
     }
 
+    /**
+     * Get the fitness of this chromosome, when evaluated in the bin packing problem.
+     * This can be used to find the accuracy of the chromosome 'model'.
+     * @return Value of all included items, zero if included items are over 10 lbs.
+     */
     public int getFitness() {
         double totalWeight = 0;
         int totalValue = 0;
@@ -58,6 +84,11 @@ public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome
         }
     }
 
+    /**
+     * Compare two Chromosomes' fitness together.
+     * @param other the object to be compared.
+     * @return -1 for less fit, 1 for more fit, 0 for same fitness.
+     */
     @Override
     public int compareTo(Chromosome other) {
         if (this.getFitness() < other.getFitness()) {
--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,10 +1,17 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GeneticAlgorithm {
-    public static ArrayList<Item> readData(String filename) throws IOException {
+    /**
+     * Reads in a file in a CSV format, and parses it into an arrayList of items.
+     * @param filename Filename to read data from
+     * @return ArrayList of items, that can be turned into a Chromosome.
+     * @throws FileNotFoundException File path of name 'filename' is not found or able to be read.
+     */
+    public static ArrayList<Item> readData(String filename) throws FileNotFoundException {
         ArrayList<Item> items = new ArrayList<>();
         File file = new File(filename);
         Scanner scanner = new Scanner(file);
@@ -22,11 +29,41 @@ public class GeneticAlgorithm {
         return items;
     }
 
+    /**
+     * Initialize population using a starter chromosome, and create a population with it, of a certain size.
+     * @param items Initial chromosome structure
+     * @param populationSize The size of the population of chromosomes.
+     * @return An arrayList of chromosomes of size 'populationSize'
+     */
     public static ArrayList<Chromosome> initializePopulation(ArrayList<Item> items, int populationSize) {
         ArrayList<Chromosome> population = new ArrayList<>();
         for (int i = 0; i < populationSize; i++) {
             population.add(new Chromosome(items));
         }
         return population;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Chromosome initialChromosome = new Chromosome(readData("test.csv"));
+        ArrayList<Chromosome> Generation_1 = initializePopulation(initialChromosome, 10);
+
+        ArrayList<Chromosome> Generation_2 = new ArrayList<>();
+        for (Chromosome c : Generation_1) {
+            Generation_2.add(new Chromosome(c));
+        }
+
+        ArrayList<Chromosome> pairOff = new ArrayList<>(Generation_2);
+        while (!pairOff.isEmpty()) {
+            Random rng = new Random();
+            int parent1;
+            int parent2;
+
+            parent1 = rng.nextInt(pairOff.size());
+
+            do {
+                parent2 = rng.nextInt(pairOff.size());
+            } while (parent1 == parent2);
+
+        }
     }
 }
