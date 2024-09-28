802f97818a8f5e361f1bd6f7c68312693b0f9fd1
Sean Fricke <seanfricke03@gmail.com>
2024-09-28 16:32:53 -0400

    Add Documentation to Chromosome class

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -2,34 +2,36 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Chromosome class, for storing a list of items, and included training functions such as
- * crossover of two Chromosomes, or mutation for a single one.
+ * CHROMOSOME class, for storing an ArrayList of ITEMS, and included training functions such as
+ * crossover of two CHROMOSOMES, or mutation for a single one.
  */
 public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome> {
-    private static Random rng = new Random();
+    private static Random rng = new Random(); // RNG init
     /**
-     * Construct an empty chromosome.
+     * Construct an empty CHROMOSOME.
      */
     public Chromosome() {
     }
 
     /**
-     * Construct a chromosome using an ArrayList of items as its structure.
-     * @param items Structure template for the new chromosome.
+     * Construct a CHROMOSOME using an ArrayList of ITEMS as its structure.
+     * @param items Structure template for the new CHROMOSOME.
      */
     public Chromosome(ArrayList<Item> items) {
         for (Item item : items) {
+            // Copy values that are in constructor
             Item itemCopy = new Item(item.getName(), item.getWeight(), item.getValue());
-            itemCopy.setIncluded(rng.nextBoolean());
-            add(itemCopy);
+
+            itemCopy.setIncluded(rng.nextBoolean()); // Copy Included state
+            add(itemCopy); // Add ITEM copy to CHROMOSOME
         }
     }
 
     /**
-     * Create a 'child' chromosome between two parents (this chromosome, and one other)
-     * @param other Second chromosome object - along with this chromosome - that will give some of their traits
-     *              to the child.
-     * @return Child chromosome with random traits from each parent.
+     * Create a 'child' CHROMOSOME between two parents (this CHROMOSOME, and one other)
+     * @param other Second CHROMOSOME object - along with this CHROMOSOME - that will give some of their traits
+     * (INCLUDED value) to the child.
+     * @return Child CHROMOSOME with random traits (INCLUDED value) from each parent.
      */
     public Chromosome crossover(Chromosome other) {
         Chromosome child = new Chromosome();
@@ -38,77 +40,87 @@ public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome
             /* Use bool result instead of int because 5 and 5 chance split (50% chance) is the equivalent to a bool
             giving 1 or 0 equally */
           boolean traitParent = rng.nextBoolean();
-          Item trait;
+
+          Item gene; // Target gene
           if (traitParent) {
-              trait = new Item(this.get(i).getName(), this.get(i).getWeight(), this.get(i).getValue());
-              trait.setIncluded(this.get(i).isIncluded());
+              // Make item with constructor needed fields, then sets INCLUDED separately.
+              gene = new Item(this.get(i).getName(), this.get(i).getWeight(), this.get(i).getValue());
+              gene.setIncluded(this.get(i).isIncluded());
           }
           else {
-              trait = new Item(other.get(i).getName(), other.get(i).getWeight(), other.get(i).getValue());
-              trait.setIncluded(other.get(i).isIncluded());
+              // Make item with constructor needed fields, then sets INCLUDED separately.
+              gene = new Item(other.get(i).getName(), other.get(i).getWeight(), other.get(i).getValue());
+              gene.setIncluded(other.get(i).isIncluded());
           }
-          child.add(trait);
+          child.add(gene); // Add gene to child CHROMOSOME.
         }
         return child;
     }
 
     /**
-     * Give this chromosome random inverses of traits at a rare chance.
+     * Give this CHROMOSOME random inverses of traits (INCLUDED value) at a rare chance.
      */
     public void mutate() {
         for (Item item : this) {
-            int mutation = new Random().nextInt(9) + 1;
+            int mutation = new Random().nextInt(9) + 1; // Pick a random Int 1-10
             if (mutation == 1) {
-                item.setIncluded(!item.isIncluded());
+                item.setIncluded(!item.isIncluded()); // Mutate if 1 was picked.
             }
         }
     }
 
     /**
-     * Get the fitness of this chromosome, when evaluated in the bin packing problem.
-     * This can be used to find the accuracy of the chromosome 'model'.
-     * @return Value of all included items, zero if included items are over 10 lbs.
+     * Get the fitness of this CHROMOSOME, when evaluated in the bin packing problem.
+     * This can be used to find the accuracy of the CHROMOSOME 'model'.
+     * @return Value of all included ITEMS, zero if included ITEMS are over 10 lbs.
      */
     public int getFitness() {
+        // Total var inits
         double totalWeight = 0;
         int totalValue = 0;
+
         for (Item item : this) {
             if (item.isIncluded()) {
-                totalWeight += item.getWeight();
-                totalValue += item.getValue();
+                totalWeight += item.getWeight(); // If INCLUDED, add on its WEIGHT to total.
+                totalValue += item.getValue(); // If INCLUDED, add on its VALUE to total.
             }
         }
+
         if (totalWeight > 10) {
-            return 0;
+            return 0; // If total WEIGHT is over 10 lbs, fitness is 0.
         } else{
-            return totalValue;
+            return totalValue; // If total WEIGHT is 10 lbs or less, fitness is the total VALUE.
         }
     }
 
     /**
-     * Compare two Chromosomes' fitness together.
-     * @param other the object to be compared.
+     * Compare two CHROMOSOMES' fitness together.
+     * @param other the CHROMOSOME to be compared.
      * @return -1 for less fit, 1 for more fit, 0 for same fitness.
      */
     @Override
     public int compareTo(Chromosome other) {
         if (this.getFitness() < other.getFitness()) {
-            return 1;
+            return 1; // If less than OTHER
         } else if (this.getFitness() > other.getFitness()) {
-            return -1;
+            return -1; // If greater than OTHER
         } else {
-            return 0;
+            return 0; // If equal to OTHER
         }
     }
 
+    /**
+     * toString for the CHROMOSOME info.
+     * @return [ITEM toString for every ITEM] Fitness: [Fitness]
+     */
     public String toString() {
-        String output = "";
+        String output = ""; // Empty String init
         for (Item item : this) {
             if (item.isIncluded()) {
-                output += item + "\n";
+                output += item + "\n"; // Add the toString of every ITEM that is INCLUDED.
             }
         }
-        output += "Fitness: " + this.getFitness() + "\n";
+        output += "Fitness: " + this.getFitness() + "\n"; // Add the fitness to the end of the String
         return output;
     }
 }
--- src/Item.java
+++ src/Item.java
@@ -7,10 +7,10 @@ public class Item {
 
 
     /**
-     * Constructor for Item that takes in explicit values as args.
-     * @param name Name of Item.
-     * @param weight Weight of Item.
-     * @param value Monetary value of Item.
+     * Constructor for ITEM that takes in explicit values as args.
+     * @param name WEIGHT of ITEM.
+     * @param weight Weight of ITEM.
+     * @param value Monetary value of ITEM.
      */
     public Item(String name, double weight, int value) {
         this.name = name;
@@ -20,8 +20,8 @@ public class Item {
     }
 
     /**
-     * Constructor for Item that takes in another Item as template, and copies it.
-     * @param other Template Item to copy.
+     * Constructor for ITEM that takes in another ITEM as template, and copies it.
+     * @param other Template ITEM to copy.
      */
     public Item(Item other){
         this.name = other.name;
@@ -30,48 +30,48 @@ public class Item {
     }
 
     /**
-     * Setter for the Included field to dictate whether the Item is taken in that scenario (Chromosome).
-     * @param included Boolean to set for Included field.
+     * Setter for the INCLUDED field to dictate whether the ITEM is taken in that scenario (Chromosome).
+     * @param included Boolean to set for INCLUDED field.
      */
     public void setIncluded(boolean included) {
         this.included = included;
     }
 
     /**
-     * Getter for item Name
-     * @return Item name as String.
+     * Getter for ITEM WEIGHT
+     * @return ITEM WEIGHT as String.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Getter for item Weight.
-     * @return Weight of item as Double.
+     * Getter for ITEM WEIGHT.
+     * @return WEIGHT of ITEM as Double.
      */
     public double getWeight() {
         return this.weight;
     }
 
     /**
-     * Getter for item Value.
-     * @return Value of items as Int.
+     * Getter for ITEM VALUE.
+     * @return VALUE of ITEMS as Int.
      */
     public int getValue() {
         return this.value;
     }
 
     /**
-     * Getter for Included.
-     * @return Included 'state' as Boolean.
+     * Getter for INCLUDED.
+     * @return INCLUDED 'state' as Boolean.
      */
     public boolean isIncluded() {
         return this.included;
     }
 
     /**
-     * To string for the item info.
-     * @return <Name> ( <Weight> lbs, $<Value>
+     * toString for the ITEM info.
+     * @return [NAME] ([WEIGHT] lbs, $[VALUE])
      */
     public String toString() {
         return this.name + " (" + this.weight + " lbs, $" + this.value + ")";
