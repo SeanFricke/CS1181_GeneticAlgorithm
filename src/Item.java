40d15119718933d1de477aa9adbbb51bb57c740a
Sean Fricke <seanfricke03@gmail.com>
2024-09-28 16:00:34 -0400

    Add final documentation to Item class

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,17 @@
 public class Item {
+    // Field Inits
     private final String name;
     private final double weight;
     private final int value;
     private boolean included;
 
 
-
-
+    /**
+     * Constructor for Item that takes in explicit values as args.
+     * @param name Name of Item.
+     * @param weight Weight of Item.
+     * @param value Monetary value of Item.
+     */
     public Item(String name, double weight, int value) {
         this.name = name;
         this.weight = weight;
@@ -14,36 +19,62 @@ public class Item {
         this.included = false;
     }
 
+    /**
+     * Constructor for Item that takes in another Item as template, and copies it.
+     * @param other Template Item to copy.
+     */
     public Item(Item other){
         this.name = other.name;
         this.weight = other.weight;
         this.value = other.value;
     }
 
+    /**
+     * Setter for the Included field to dictate whether the Item is taken in that scenario (Chromosome).
+     * @param included Boolean to set for Included field.
+     */
     public void setIncluded(boolean included) {
         this.included = included;
     }
 
+    /**
+     * Getter for item Name
+     * @return Item name as String.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for item Weight.
+     * @return Weight of item as Double.
+     */
     public double getWeight() {
         return this.weight;
     }
 
+    /**
+     * Getter for item Value.
+     * @return Value of items as Int.
+     */
     public int getValue() {
         return this.value;
     }
 
+    /**
+     * Getter for Included.
+     * @return Included 'state' as Boolean.
+     */
     public boolean isIncluded() {
         return this.included;
     }
 
-
+    /**
+     * To string for the item info.
+     * @return <Name> ( <Weight> lbs, $<Value>
+     */
     public String toString() {
-        String out = this.name + " (" + this.weight + " lbs, $" + this.value + ")";
-        return out;
+        return this.name + " (" + this.weight + " lbs, $" + this.value + ")";
     }
 
 }
